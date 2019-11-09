c4bd8233c015c69d1d8cbd7b5e129e019fc1951d
FSuaverdez <57562242+FSuaverdez@users.noreply.github.com>
2019-11-09 20:20:44 +0800

    Uploaded FIles

---
+++ src/Node.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author asus
+ */
+public class Node {
+
+    String productID;
+    String productName;
+    int price;
+    Node next;
+}
+
+class LinkedList {
+
+    Node head = null;
+
+    void insert(String productID, String productName, int price) {
+
+        Node temp = new Node();
+        temp = head;
+
+        if (head == null) {
+            head.productID = productID;
+            head.productName = productName;
+            head.price = price;
+
+        } else {
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp = temp.next;
+            temp.productID = productID;
+            temp.productID = productName;
+            temp.price = price;
+
+        }
+
+    }
+}
